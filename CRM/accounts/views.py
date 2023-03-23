80dcbedda7cf73da6c74a2e420c19f177a5ea62c
Javio Felix <javifsk@gmail.com>
2023-03-23 21:05:28 +0800

    Add user registration and Login

--- CRM/accounts/views.py
+++ CRM/accounts/views.py
@@ -1,13 +1,59 @@
-from re import M
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.contrib import messages
 from .models import Product,Customer,Order,Tag
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request,f"Account was created suxccessfully - {user}")
+
+                return redirect('login')
+
+    context = {'form':form}
+    return render(request,'accounts/register.html',context)
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request,username=username,password=password)
+
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or password is incorrect')
+                # return redirect('login')
+
+    context = {}
+    return render(request,'accounts/login.html',context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -22,6 +68,8 @@ def home(request):
     "pending":pending}
     return render(request,'accounts/dashboard.html',context)
 
+
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
 
